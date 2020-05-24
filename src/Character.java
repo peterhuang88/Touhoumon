8448745c578f1e453615a1e319ff0a7a459cda1f
Huggietron <48580675+Huggietron@users.noreply.github.com>
2020-05-23 22:12:12 -0700

    Add -character class -player class

---
+++ src/Character.java
@@ -0,0 +1,7 @@
+public class Character {
+    private int hp;
+    private int pwr;
+    private int atk;
+    private int def;
+    private int spd;
+}
---
+++ src/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    private int xPos;
+    private int yPos;
+    private int facing;
+    private Character main;
+    private Character[] support;
+}
